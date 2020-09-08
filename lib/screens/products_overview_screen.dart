1a4e1b72f2ec83266bb1504a547a32b14bf2fde1
franklaercio <franklaercio@gmail.com>
2020-09-08 00:03:36 -0300

    Adding cart addItem

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
+import '../providers/cart.dart';
 
 enum FilterOptions {
   Favorites,
@@ -42,7 +45,17 @@ class _ProductOverViewScreenState extends State<ProductOverViewScreen> {
               )
             ],
             icon: Icon(Icons.more_vert),
-          )
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: null,
+            ),
+          ),
         ],
       ),
       body: ProductsGrid(_showOnlyFavorites),

b85aa8d35fa4a6e32cfe88fa5cbc4df5a63c8801
Frank Laercio <franklaercio@gmail.com>
2020-10-31 01:51:50 -0300

    Fixing imports

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -4,7 +4,6 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/user_product_item.dart';
-import '../widgets/app_drawer.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';

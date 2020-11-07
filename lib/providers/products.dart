9d76cb64f3e715af34a615e97c6bf1e26977b4ef
Frank Laercio <franklaercio@gmail.com>
2020-11-07 00:32:28 -0300

    Adding save product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -66,8 +66,14 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct() {
-    //_items.add(null);
+  void addProduct(Product product) {
+    final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        imageUrl: product.imageUrl,
+        price: product.price,
+        id: DateTime.now().toString());
+    _items.add(newProduct);
     notifyListeners();
   }
 }

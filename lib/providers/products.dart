e03c4d0a99ec933f5ff66b85b608e462045bdf4d
Frank Laercio <franklaercio@gmail.com>
2020-11-18 23:38:05 -0300

    Implementing fetch products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -67,6 +67,17 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
+  Future<void> fetchAndSetProducts() async {
+    const url = 'PUT YOUR FIREBASE CONNECTION HERE';
+
+    try {
+      final response = await http.get(url);
+      print(json.decode(response.body));
+    } catch (error) {
+      throw (error);
+    }
+  }
+
   Future<void> addProduct(Product product) async {
     const url = 'PUT YOUR FIREBASE CONNECTION HERE';
 
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -6,6 +6,7 @@ import '../widgets/app_drawer.dart';
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
+import '../providers/products.dart';
 
 enum FilterOptions {
   Favorites,
@@ -19,6 +20,28 @@ class ProductOverViewScreen extends StatefulWidget {
 
 class _ProductOverViewScreenState extends State<ProductOverViewScreen> {
   var _showOnlyFavorites = false;
+  var _isInit = true;
+
+  @override
+  void initState() {
+    //Provider.of<Products>(context).fetchAndSetProducts(); // WON'T WORK!
+    //Future.delayed(Duration.zero).then((_) {
+    //  Provider.of<Products>(context).fetchAndSetProducts();
+    //});
+
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      Provider.of<Products>(context).fetchAndSetProducts();
+    }
+
+    _isInit = false;
+
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
